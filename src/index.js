0b875eee2c9d40d96449d0ef9157ff7628a282c5
Afeez G Lawal <afeezlg@gmail.com>
2022-02-02 08:58:48 +0100

    edited index.html name and description meta tags, edited manifest.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import bootstrap from "bootstrap";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
